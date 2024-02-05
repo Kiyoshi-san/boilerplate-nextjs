d25369113af75bca6ebc4634b5c20a3c5ff04d56
Kiyoshi <kiyoshibudokan@hotmail.com>
2024-02-05 14:08:23 -0300

    Downgrading Nextjs to version 13 since the version 14 was breaking when used with babel

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
     styledComponents: true,
